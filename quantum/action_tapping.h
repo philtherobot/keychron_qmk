3e1b3fd80b0a97f8b5d2ebabf77ec23fe236600b
Philippe Payant <philippe.payant@microdrones.com>
2023-11-08 20:31:25 -0500

    My first batch of changes

--- quantum/action_tapping.h
+++ quantum/action_tapping.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* tap count needed for toggling a feature */
 #ifndef TAPPING_TOGGLE
-#    define TAPPING_TOGGLE 5
+#    define TAPPING_TOGGLE 1
 #endif
 
 #define WAITING_BUFFER_SIZE 8
